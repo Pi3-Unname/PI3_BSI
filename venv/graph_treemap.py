1c6b9519c8ef9f529c72c44fa39a286f5e6d6a53
José Carlos <jose85751696@gmail.com>
2023-07-29 13:52:06 -0300

    Update graph_treemap.py

--- venv/graph_treemap.py
+++ venv/graph_treemap.py
@@ -24,34 +24,40 @@ def graph_treemap(data, options, default=[]):
     st.plotly_chart(px.treemap(data, path=select))
 
 def graph(data: pd.DataFrame, x: str, options: np.ndarray, type_graph, type_txt: str):
-    col1, col2 = st.columns((3, 2))
-    st.markdown(f'## {type_txt.title()}')
-    options = options.tolist()
-    options.remove(x)
-    options.insert(-1, 'Total')
-    options.insert(0, '')
-    with col1:
-        st.write(f"Selecione um campo para interagir com o campo")
-        select = st.selectbox(
-            f"{x}{type_graph}",
-            options=options,
-            index=0,
-            label_visibility='hidden'
-        )
-    with col2:
-        st.write('Escolha o campo para separar as cores ')
-        color = st.selectbox(
-            f"{x}{type_txt}",
-            options=options,
-            label_visibility='hidden'
-        )
+  st.markdown(f'## {type_txt.title()}')
+  col1, col2 = st.columns((3, 2))
+  options = options.tolist()
+  options.remove(x)
+  options.insert(-1, 'Total')
+  options.insert(0, '')
+  with col1:
+    st.write(f"Selecione um campo para interagir com o campo")
+    select = st.selectbox(
+      f"{x}{type_graph}",
+      options=options,
+      index=0,
+      label_visibility='hidden'
+    )
+  with col2:
+    st.write('Escolha o campo para separar as cores ')
+    color = st.selectbox(
+      f"{x}{type_txt}",
+      options=options,
+      label_visibility='hidden'
+    )
 
-    if select != '':
+  if select != '':
+    if select != 'Total':
       new_data = data.groupby([x, select]).size().reset_index(name='Total')
-      if color == '':
-          st.plotly_chart(type_graph(new_data, x=x, y=select))
-      elif color == 'Total':
-          st.plotly_chart(type_graph(new_data, x=x, y=select, color=color))
+    else:
+      new_data = data.groupby([x]).size().reset_index(name='Total')
+    if color == '':
+      st.plotly_chart(type_graph(new_data, x=x, y=select))
+    elif color == 'Total':
+      st.plotly_chart(type_graph(new_data, x=x, y=select, color=color))
+    else:
+      if select != 'Total':
+        new_data = data.groupby([x, select, color]).size().reset_index(name='Total')
       else:
-          new_data = data.groupby([x, select, color]).size().reset_index(name='Total')
-          st.plotly_chart(type_graph(new_data, x=x, y=select, color=color))
+        new_data = data.groupby([x, color]).size().reset_index(name='Total')
+      st.plotly_chart(type_graph(new_data, x=x, y=select, color=color))
